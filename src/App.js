7fe4ceeaa69e4fbd33eb7d62aec21bd3ef5924a0
100mgml@gmail.com <100mgml@gmail.com>
2023-02-06 15:10:09 +0900

    feat : Implement product list page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import productData from './data.js';
+import { useState } from 'react';
 
 function App() {
+  let [dataList, setDataList] = useState(productData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="container">
+        <div className="row">
+          {dataList.map((data, i) => {
+            return <DataCard data={data}></DataCard>;
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function DataCard(props) {
+  return (
+    <div className="col-md-4" key={props.data.title}>
+      <img src="logo512.png" width="80%" alt={props.data.title} />
+      <h4>{props.data.title}</h4>
+      <p>{props.data.content}</p>
     </div>
   );
 }
